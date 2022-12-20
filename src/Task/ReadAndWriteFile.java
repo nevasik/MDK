74dd9e1b1dac52caffb71a29e7ef56b657715573
nevasik <nevasik@mail.ru>
2022-12-20 21:47:52 +0300

    Добавления комментариев и написания javadoc

--- src/Task/ReadAndWriteFile.java
+++ src/Task/ReadAndWriteFile.java
@@ -3,54 +3,65 @@ import java.io.*;
 import java.util.*;
 
 /**
-Студент Группа Дисциплина Оценки
-Структура входного файла in.txt
-Тигра;Г1;Физика; 3 3 5
-Винни_Пух;Г2;Пчеловодство;5 5 5
-Винни_Пух;Г2;Русский язык;3 3
-КроликГ1;Русский язык; 5 5 5 4
-Тигра;Г1;Химия; 3 4 5
-
-Сформировать список студентов с любимыми дисциплинами, исходя из среднего балла (гарантируется, что для каждого студента только одна любимая дисциплина).
-Список упорядочить по названию группы и по фамилии студента
-        Структура выходного файла out.txt
-        Г1 Кролик Русский язык
-        Г1 Тигра Химия
-        Г2 Винни_Пух Пчеловодство
- **/
+ * @author Dima Poplaukhin
+ * @version 1.1
+ * @since 1.0
+ * <string>Это класс ReadAndWriteFile, тут будет происходить решение задачи на считывание, сортировку, обработку данных, запись в файл</string>
+ */
 public class ReadAndWriteFile {
 
+    /**
+     * Стартовая точка программы.
+     * @param args тут происходит выполнение основой программы
+     * @throws IOException при неудачном выполнении программы, может выбросить IOException(исключение)
+     */
+
     public static void main(String[] args) throws IOException {
-        File fileIn = new File("in");
-        Scanner scanner = new Scanner(fileIn);
-        final List<Student> records = new ArrayList<>();
-        while (scanner.hasNextLine()){
-            String[] fields = scanner.nextLine().split(";");
-            OptionalDouble averageGradeDouble = Arrays.stream(fields[3].split(" "))
+        File fileIn = new File("in"); // создаём объект File и в аргументе класса пишем название файла "in"
+        readWriteFiles(fileIn);
+    }
+
+    /**
+     * <string>Происходит запись из файла, сохранение значений в объект класса, сохранений в список, сортировка данных и запись в файл</string>
+     * @param file это файловое значение, эта функция не возрващает ничего, и принимает аргумент метода File
+     * @throws IOException при неудачном считывании может опять выбрасить IOException(исключение)
+     */
+    private static void readWriteFiles(File file) throws IOException {
+        Scanner scanner = new Scanner(file);          // создали Scanner что бы произвести считывание файла
+        final List<Student> records = new ArrayList<>(); // обьявили коллекцию, которая принимает объекты класса Student
+
+        while (scanner.hasNextLine()) {                 // цикл, который считывает файл по-строчно
+            String[] fields = scanner.nextLine().split(";"); // делаем split, и указываем в аргументе метода, через что разделяются значения в файле
+
+            OptionalDouble averageGradeDouble = Arrays.stream(fields[3].split(" ")) // в строке берём последнее значение где оценки студента, переводим в число и находим сред.знач.
                     .mapToInt(Integer::parseInt)
                     .average();
-            double averageGrade = averageGradeDouble.isPresent() ? averageGradeDouble.getAsDouble() : 0;
 
-            Student student = new Student(fields[0], fields[1], fields[2], averageGrade);
-            records.add(student);
+            double averageGrade = averageGradeDouble.isPresent() ? averageGradeDouble.getAsDouble() : 0; // проверяем на пустоту(true-ср.знач; false - 0)
+
+            Student student = new Student(fields[0], fields[1], fields[2], averageGrade); // создаём объект класса Student, разложили по полям значения их файла
+
+            records.add(student); // добавляем в нашу коллекцию объект класса Student, у которого значения полей - это значения строки
         }
 
-        Comparator<Student> averageComparator = Comparator.comparingDouble(Student::getAverage).reversed();
-        List<Student> finalRecords = records.stream().sorted(
-                        Comparator.comparing(Student::getGroup)
-                                .thenComparing(Student::getStudent)
-                                .thenComparing(averageComparator))
-                .distinct()
-                .toList();
+        Comparator<Student> averageComparator = Comparator.comparingDouble(Student::getAverage).reversed(); // создаём Comparator, что бы сделать сортировку по полям названия группы и по фамилиям студентов
 
-        FileWriter fileWriter = new FileWriter("Out");
-        for(Student student : finalRecords) {
-            fileWriter.write(student.getGroup() + " "
+        List<Student> finalRecords = records.stream().sorted(
+                Comparator.comparing(Student::getGroup)
+                        .thenComparing(Student::getStudent)
+                        .thenComparing(averageComparator))
+                        .distinct() // удаляем дубликаты
+                        .toList(); // возводим в коллекцию List
+
+        FileWriter fileWriter = new FileWriter("Out"); // Создали FileWriter, что бы записать в файл Out предметы, где больше ср.бал, отсортированные значения каждого студента
+
+        for(Student student : finalRecords) { // итерируемся по отсортированной коллекции List
+            fileWriter.write(student.getGroup() + " " // записываем в файл группу, студента, дисциплину(любимый предмет студента), и в конце добавили Сепаратор, что бы наши значения не слиплись в одну строку
                     + student.getStudent() + " "
                     + student.getDiscipline()
                     + System.lineSeparator());
         }
-        fileWriter.close();
-    }
 
+        fileWriter.close(); // закрываем потоки, что бы исполняемый файл не кушал лишние/ненужные ресурсы
+    }
 }
--- src/Task/Student.java
+++ src/Task/Student.java
@@ -3,45 +3,43 @@ package Task;
 import java.util.Objects;
 
 public class Student {
-    private final String student;
-    private final String group;
-    private final String discipline;
-    private final double average;
+    private final String student; // имя/прозвище студента
+    private final String group; // группа студента
+    private final String discipline; // предмет/дисциплина студента
+    private final double average; // ср.балл студента
 
-    public Student(String student, String group, String discipline, double average) {
-        this.student = student;
-        this.group = group;
-        this.discipline = discipline;
-        this.average = average;
+    public Student(String student, String group, String discipline, double average) { // базовый конструктор класса
+        this.student = student; // иницилизация полей объекта класса Student
+        this.group = group; // иницилизация полей объекта класса Student
+        this.discipline = discipline; // иницилизация полей объекта класса Student
+        this.average = average; // иницилизация полей объекта класса Student
     }
 
-    public String getStudent() {
+    public String getStudent() { // обычный геттер для класса, нужен что бы обращаться к полю в других классах
         return student;
     }
 
-    public String getGroup() {
+    public String getGroup() { // обычный геттер для класса, нужен что бы обращаться к полю в других классах
         return group;
     }
 
-    public String getDiscipline() {
+    public String getDiscipline() { // обычный геттер для класса, нужен что бы обращаться к полю в других классах
         return discipline;
     }
 
-    public double getAverage() {
+    public double getAverage() { // обычный геттер для класса, нужен что бы обращаться к полю в других классах
         return average;
     }
-
-
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) { // переопределили метод из под главного класса Object, родителя всех классов метод equals, что бы различались наши студенты
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Student student = (Student) o;
-        return getStudent().equals(student.getStudent());
+        return getStudent().equals(student.getStudent()); // делаем сравнение объектов по имени/прозвищу студента
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode() { // этот метод мы тоже переопределили как и equals, по hashCode сравниваются объекты. У каждого объекта свой hashCode
         return Objects.hash(getStudent());
     }
 }
